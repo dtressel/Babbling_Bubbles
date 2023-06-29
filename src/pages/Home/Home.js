3d3d94842466090efbe4b0a56d907274f2e45eab
Daniel Tressel <dhtressel@gmail.com>
2023-06-29 08:18:14 -0400

    created simple home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,21 @@
+import Button from '@mui/material/Button';
+import './Home.css';
+
 const Home = () => {
   return (
-    <>
-      <p>This is the home page.</p>
-    </>
+    <div className="Home">
+      <p>Play Babbling Bubbles now!</p>
+      <Button
+        variant="contained"
+        href="/play"
+        sx={{
+          background: "linear-gradient(90deg, rgba(9,161,182,1) 0%, rgba(0,212,255,1) 50%, rgba(9,161,182,1) 100%)",
+          width: "6.4rem",
+          height: "3.2rem",
+          borderRadius: "0.5rem"
+        }}
+      >Play</Button>
+    </div>
   )
 }
 
