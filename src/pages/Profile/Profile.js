3da98c06c93224befe356bce45cd39304138a027
Daniel Tressel <dhtressel@gmail.com>
2023-08-06 07:58:13 -0400

    started profile page

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,5 +1,20 @@
-const Profile = () => {
+import { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import UserContext from '../../contexts/UserContext';
+import './Profile.css';
 
+const Profile = () => {
+  const { currentUser } = useContext(UserContext);
+  const { username } = useParams(); 
+
+  return (
+    <div className="Profile-card-wrapper">
+      <div className="Profile-card rounded-paper">
+        <h3>{username}</h3>
+      </div>
+    </div>
+
+  )
 }
 
 export default Profile;
